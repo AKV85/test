77071622985e157c6c3797845bd69fd6bb1b53f3
AKV85 <a.kotov.laknojus@gmail.com>
2023-02-02 00:03:32 +0200

    Initial commit

---
+++ laravel_failai/app/Models/Listing.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+class Listing
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'one',
+                'description' => 'wpdkwodowd'
+            ],
+            [
+                'id' => 2,
+                'title' => 'two',
+                'description' => 'wpdkwodonjexjexnewd'
+            ]
+
+
+        ];
+    }
+
+    public static function find($id)
+    {
+        $listings = self::all();
+
+        foreach ($listings as $listing) {
+            if ($listing['id'] == $id){
+                return $listing;
+            }
+        }
+    }
+}
+
