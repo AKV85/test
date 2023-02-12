cc287d15c9b41078cd4da93db61f4bc56a5a7b08
AKV85 <a.kotov.laknojus@gmail.com>
2023-02-12 19:21:30 +0200

    user registration,auth links

--- laravel_failai/resources/views/components/layout.blade.php
+++ laravel_failai/resources/views/components/layout.blade.php
@@ -29,21 +29,41 @@
 </head>
 <body class="mb-48">
 <nav class="flex justify-between items-center mb-4">
-    <a href="/"
-    ><img class="logo w-36" src="{{asset('images/logo.jpg')}}" alt=""
-        /></a>
+    <a href="/" >
+        <img class="logo w-36"
+          src="{{asset('images/logo.jpg')}}"
+          alt="/">
+    </a>
     <ul class="flex space-x-6 mr-6 text-lg">
+        @auth
         <li>
-            <a href="register.html" class="hover:text-laravel"
-            ><i class="fa-solid fa-user-plus"></i> Registracija</a
-            >
+           <span class="font-bold uppercase">
+            Welcome {{auth()->user()->name}}
+           </span>
         </li>
         <li>
-            <a href="login.html" class="hover:text-laravel"
-            ><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                Prisijungimas</a
-            >
+            <a href="/listings/manage"
+               class="hover:text-laravel">
+                <i class="fa-solid fa-gear"></i>
+                ManageListings
+            </a>
         </li>
+        @else
+        <li>
+            <a href="/register"
+               class="hover:text-laravel">
+                <i class="fa-solid fa-user-plus"></i>
+                Registracija
+            </a>
+        </li>
+        <li>
+            <a href="/login"
+               class="hover:text-laravel">
+                <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                Prisijungimas
+            </a>
+        </li>
+            @endauth
     </ul>
 </nav>
 
--- laravel_failai/routes/web.php
+++ laravel_failai/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -53,6 +54,13 @@ Route::delete('/listings/{listing}',
 Route::get('/listings/{listing}',
     [ListingController::class,'show']);
 
+//show register/create form
+Route::get('/register',
+    [UserController::class,'create']);
+
+//create new user
+Route::post('/users',
+    [UserController::class,'store']);
 
 
 
